f80a6298cd3dfdc013ef618dae78c898860d11ee
Guilherme Ignácio <stormierearth9@gmail.com>
2024-09-20 20:27:47 -0300

    Show Questions Buttons

--- app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainScreen.kt
+++ app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainScreen.kt
@@ -1,13 +1,35 @@
 package com.guilherme.braintappers.presentation.screen.trivia
 
+import android.text.Html
+import android.text.Spanned
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.guilherme.braintappers.R
+import com.guilherme.braintappers.util.poppinsFamily
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -20,22 +42,73 @@ fun TriviaMainScreen(
 ) {
 
     val viewModel = koinViewModel<TriviaMainViewModel>()
+    val state by viewModel.state.collectAsStateWithLifecycle()
+    val onEvent = viewModel::onEvent
 
     LaunchedEffect(Unit) {
         viewModel.fetchTrivia(
             numberOfQuestions = numberOfQuestions,
-            categoryId = "",
+            categoryId = "", //Todo: Get Category
             difficulty = difficulty,
             type = type
         )
     }
 
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .statusBarsPadding()) {
-        Text(text = numberOfQuestions)
-        Text(text = difficulty)
-        Text(text = type)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .statusBarsPadding()
+    ) {
+//        LinearProgressIndicator(progress = {  })
+
+        IconButton(onClick = { navController.navigateUp() }) {
+            Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "")
+        }
+
+        state.questions?.let { question ->
+
+            val answers = question[state.currentQuestion].incorrectAnswers.toMutableStateList()
+            answers.add(question[state.currentQuestion].correctAnswer)
+
+
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = question[state.currentQuestion].question.parseHtml(),
+                fontSize = MaterialTheme.typography.titleLarge.fontSize,
+                fontWeight = FontWeight.Bold,
+                fontFamily = poppinsFamily
+            )
+
+            answers.map { it.parseHtml() }.shuffled().forEach{
+                OutlinedButton(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = {
+                        UserTrivia(
+                            correctAnswer = question[state.currentQuestion].correctAnswer,
+                            selectedAnswer = it
+                        )
+                    }
+                ) {
+                    Text(text = it)
+                }
+            }
+
+        }
+
+        Button(onClick = { onEvent(TriviaMainEvents.NextQuestion) }) {
+            Text(text = "Next Question")
+        }
+
     }
 }
+
+fun String.parseHtml(): String {
+    return Html.fromHtml(this, Html.FROM_HTML_MODE_LEGACY).toString()
+}
+
+data class UserTrivia(
+    val correctAnswer: String,
+    val selectedAnswer: String
+)
 
--- app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainViewModel.kt
+++ app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainViewModel.kt
@@ -1,11 +1,31 @@
 package com.guilherme.braintappers.presentation.screen.trivia
 
 import androidx.lifecycle.ViewModel
+import com.guilherme.braintappers.domain.DataError
+import com.guilherme.braintappers.domain.Result
 import com.guilherme.braintappers.domain.TriviaApiService
+import com.guilherme.braintappers.domain.model.ApiResponse
+import com.guilherme.braintappers.domain.model.Question
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+data class TriviaMainState(
+    val questions: List<Question>? = null,
+    val currentQuestion: Int = 0
+)
+
+sealed interface TriviaMainEvents {
+    data object PreviousQuestion : TriviaMainEvents
+    data object NextQuestion : TriviaMainEvents
+}
 
 class TriviaMainViewModel(
     private val trivia: TriviaApiService
 ) : ViewModel() {
+
+    private val _state = MutableStateFlow(TriviaMainState())
+    val state = _state.asStateFlow()
 
     suspend fun fetchTrivia(
         numberOfQuestions: String,
@@ -13,14 +33,48 @@ class TriviaMainViewModel(
         difficulty: String,
         type: String
     ) {
-        println(
-            trivia.fetchTriviaByCategory(
-                numberOfQuestions = numberOfQuestions,
-                categoryId = categoryId,
-                difficulty = difficulty,
-                type = type,
-            )
+
+        val lorem = trivia.fetchTriviaByCategory(
+            numberOfQuestions = numberOfQuestions,
+            categoryId = categoryId,
+            difficulty = difficulty,
+            type = type,
         )
+
+        when (lorem) {
+            is Result.Success -> {
+                _state.update {
+                    it.copy(
+                        questions = lorem.data.results,
+                    )
+                }
+            }
+
+            is Result.Error -> {
+                println("ERROR FETCHING DATA:" + lorem.error)
+            }
+        }
+
+    }
+
+    fun onEvent(event: TriviaMainEvents) {
+        when (event) {
+            TriviaMainEvents.PreviousQuestion -> {
+                _state.update {
+                    it.copy(
+                        currentQuestion = _state.value.currentQuestion - 1
+                    )
+                }
+            }
+
+            TriviaMainEvents.NextQuestion -> {
+                _state.update {
+                    it.copy(
+                        currentQuestion = _state.value.currentQuestion + 1
+                    )
+                }
+            }
+        }
     }
 
 }
