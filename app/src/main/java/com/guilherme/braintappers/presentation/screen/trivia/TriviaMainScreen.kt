36f6bbc24ee27d2fc79a06ee76eed206f0ace2e0
Guilherme Ignácio <stormierearth9@gmail.com>
2024-09-21 16:02:03 -0300

    Save User Answers

--- app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainScreen.kt
+++ app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainScreen.kt
@@ -1,34 +1,32 @@
 package com.guilherme.braintappers.presentation.screen.trivia
 
 import android.text.Html
-import android.text.Spanned
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
-import com.guilherme.braintappers.R
+import com.guilherme.braintappers.domain.model.Question
+import com.guilherme.braintappers.ui.theme.primaryColor
 import com.guilherme.braintappers.util.poppinsFamily
 import org.koin.androidx.compose.koinViewModel
 
@@ -52,6 +50,7 @@ fun TriviaMainScreen(
             difficulty = difficulty,
             type = type
         )
+
     }
 
     Column(
@@ -64,13 +63,13 @@ fun TriviaMainScreen(
         IconButton(onClick = { navController.navigateUp() }) {
             Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "")
         }
-
         state.questions?.let { question ->
 
-            val answers = question[state.currentQuestion].incorrectAnswers.toMutableStateList()
-            answers.add(question[state.currentQuestion].correctAnswer)
-
-
+            val answers = remember(state.currentQuestion) {
+                val answers = question[state.currentQuestion].incorrectAnswers.toMutableList()
+                answers.add(question[state.currentQuestion].correctAnswer)
+                answers.shuffled().map { it.parseHtml() }
+            }
 
             Text(
                 modifier = Modifier.fillMaxWidth(),
@@ -80,25 +79,51 @@ fun TriviaMainScreen(
                 fontFamily = poppinsFamily
             )
 
-            answers.map { it.parseHtml() }.shuffled().forEach{
+
+            answers.forEach {
                 OutlinedButton(
                     modifier = Modifier.fillMaxWidth(),
+                    shape = RoundedCornerShape(20),
+                    colors = ButtonDefaults.outlinedButtonColors(
+                        contentColor = Color.Black,
+                        containerColor = if (state.selectedAnswer == it) primaryColor else Color.Transparent,
+                    ),
                     onClick = {
-                        UserTrivia(
-                            correctAnswer = question[state.currentQuestion].correctAnswer,
+                        val userTrivia = UserTrivia(
+                            question = question[state.currentQuestion].question.parseHtml(),
+                            correctAnswer = question[state.currentQuestion].correctAnswer.parseHtml(),
                             selectedAnswer = it
                         )
+
+                        onEvent(TriviaMainEvents.OnAnswerClicked(userTrivia))
+
                     }
                 ) {
                     Text(text = it)
                 }
             }
 
+            Button(onClick = { onEvent(TriviaMainEvents.PreviousQuestion) }) {
+                Text(text = "Previous Question")
+            }
+            Button(
+                onClick = {
+                    if (state.currentQuestion != question.size - 1) onEvent(TriviaMainEvents.NextQuestion) else {/*Todo: Finish Quiz*/
+                    }
+                }
+            ) {
+
+                AnimatedContent(
+                    targetState = state.currentQuestion == question.size - 1
+                ) {
+                    Text(text = if (!it) "Next Question" else "Finish")
+                }
+
+            }
+
+
         }
 
-        Button(onClick = { onEvent(TriviaMainEvents.NextQuestion) }) {
-            Text(text = "Next Question")
-        }
 
     }
 }
@@ -108,6 +133,7 @@ fun String.parseHtml(): String {
 }
 
 data class UserTrivia(
+    val question: String,
     val correctAnswer: String,
     val selectedAnswer: String
 )
--- app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainViewModel.kt
+++ app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainViewModel.kt
@@ -1,10 +1,8 @@
 package com.guilherme.braintappers.presentation.screen.trivia
 
 import androidx.lifecycle.ViewModel
-import com.guilherme.braintappers.domain.DataError
 import com.guilherme.braintappers.domain.Result
 import com.guilherme.braintappers.domain.TriviaApiService
-import com.guilherme.braintappers.domain.model.ApiResponse
 import com.guilherme.braintappers.domain.model.Question
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -12,10 +10,13 @@ import kotlinx.coroutines.flow.update
 
 data class TriviaMainState(
     val questions: List<Question>? = null,
-    val currentQuestion: Int = 0
+    val currentQuestion: Int = 0,
+    val answeredQuestions: MutableList<UserTrivia> = mutableListOf(),
+    val selectedAnswer: String? = null
 )
 
 sealed interface TriviaMainEvents {
+    data class OnAnswerClicked(val value: UserTrivia) : TriviaMainEvents
     data object PreviousQuestion : TriviaMainEvents
     data object NextQuestion : TriviaMainEvents
 }
@@ -73,6 +74,29 @@ class TriviaMainViewModel(
                         currentQuestion = _state.value.currentQuestion + 1
                     )
                 }
+            }
+
+            is TriviaMainEvents.OnAnswerClicked -> {
+
+                val answeredQuestionsList = _state.value.answeredQuestions
+
+                val existingIndex =
+                    answeredQuestionsList.indexOfFirst { it.question == event.value.question }
+
+                if (existingIndex != -1) {
+                    // If exists, overwrite for the new one
+                    answeredQuestionsList[existingIndex] = event.value
+                } else {
+                    // If does not exists, add new
+                    answeredQuestionsList.add(event.value)
+                }
+
+                _state.update { it.copy(
+                    selectedAnswer = event.value.selectedAnswer
+                ) }
+
+                println(_state.value.answeredQuestions)
+
             }
         }
     }
