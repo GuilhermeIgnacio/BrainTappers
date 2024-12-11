7a2b0fd6230abde3f8d50e01bb0c0ef98c8b5a86
Guilherme Ignácio <stormierearth9@gmail.com>
2024-12-11 00:26:10 -0300

    Close Trivia Dialog

--- app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainScreen.kt
+++ app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainScreen.kt
@@ -14,32 +14,43 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.ProgressIndicatorDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
@@ -47,10 +58,13 @@ import com.guilherme.braintappers.R
 import com.guilherme.braintappers.domain.DataError
 import com.guilherme.braintappers.domain.DisplayResult
 import com.guilherme.braintappers.domain.model.Question
+import com.guilherme.braintappers.navigation.HomeScreen
 import com.guilherme.braintappers.ui.theme.primaryColor
 import com.guilherme.braintappers.util.poppinsFamily
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TriviaMainScreen(
     navController: NavHostController,
@@ -63,6 +77,8 @@ fun TriviaMainScreen(
     val viewModel = koinViewModel<TriviaMainViewModel>()
     val state by viewModel.state.collectAsStateWithLifecycle()
     val onEvent = viewModel::onEvent
+
+    var isDialogOpen by remember { mutableStateOf(false) }
 
     LaunchedEffect(Unit) {
         viewModel.fetchTrivia(
@@ -77,7 +93,65 @@ fun TriviaMainScreen(
     state.result?.DisplayResult(
         onSuccess = {
 
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .statusBarsPadding(),
+            ) {
+                IconButton(onClick = {
+                    isDialogOpen = !isDialogOpen
+                }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = ""
+                    )
+                }
+            }
 
+            if (isDialogOpen) {
+                AlertDialog(
+                    title = {
+                        Text(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = "Exit?",
+                            fontWeight = FontWeight.Bold,
+                            textAlign = TextAlign.Center,
+                            fontFamily = poppinsFamily
+                        )
+                    },
+                    text = {
+                        Text(
+                            text = "Are You Sure You Want To Exit? Your Progress Will Be Lost.",
+                            fontFamily = poppinsFamily
+                        )
+                    },
+                    onDismissRequest = { isDialogOpen = false },
+                    confirmButton = {
+                        TextButton(
+                            onClick = {
+                                // Had to do it this way since when using ViewModel the dialog would
+                                // survive for about 0.5 seconds at the HomeScreen
+                                isDialogOpen = false
+                                navController.navigate(HomeScreen)
+                            }
+                        ) {
+                            Text(
+                                text = "Close",
+                                fontFamily = poppinsFamily,
+                                color = Color.Black
+                            )
+                        }
+                    },
+                    dismissButton = {
+                        TextButton(
+                            onClick = { isDialogOpen = false },
+                        ) {
+                            Text(text = "Cancel", fontFamily = poppinsFamily, color = Color.Black)
+                        }
+                    }
+
+                )
+            }
 
         },
         onError = {
@@ -95,7 +169,7 @@ fun TriviaMainScreen(
                     contentDescription = "Error Icon"
                 )
 
-                when(it){
+                when (it) {
                     DataError.Response.UNKNOWN -> {
 
                     }
--- app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainViewModel.kt
+++ app/src/main/java/com/guilherme/braintappers/presentation/screen/trivia/TriviaMainViewModel.kt
@@ -1,28 +1,27 @@
 package com.guilherme.braintappers.presentation.screen.trivia
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavHostController
 import com.guilherme.braintappers.domain.DataError
 import com.guilherme.braintappers.domain.Result
 import com.guilherme.braintappers.domain.TriviaApiService
 import com.guilherme.braintappers.domain.model.ApiResponse
 import com.guilherme.braintappers.domain.model.Question
+import com.guilherme.braintappers.navigation.HomeScreen
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 data class TriviaMainState(
     val questions: List<Question>? = null,
     val result: Result<ApiResponse, DataError>? = null,
-    val currentQuestion: Int = 0,
-    val answeredQuestions: MutableList<UserTrivia> = mutableListOf(),
-    val selectedAnswer: List<String> = mutableListOf("", "", "", "", "", "", "", "", "", "")
 )
 
 sealed interface TriviaMainEvents {
-    data class OnAnswerClicked(val value: UserTrivia) : TriviaMainEvents
-    data object PreviousQuestion : TriviaMainEvents
-    data object NextQuestion : TriviaMainEvents
-    data object FinishTrivia: TriviaMainEvents
+
 }
 
 class TriviaMainViewModel(
@@ -39,28 +38,30 @@ class TriviaMainViewModel(
         type: String
     ) {
 
-        val lorem = trivia.fetchTriviaByCategory(
+        val trivia = trivia.fetchTriviaByCategory(
             numberOfQuestions = numberOfQuestions,
             categoryId = categoryId,
             difficulty = difficulty,
             type = type,
         )
 
-        _state.update { it.copy(
-            result = lorem
-        ) }
+        _state.update {
+            it.copy(
+                result = trivia
+            )
+        }
 
-        when (lorem) {
+        when (trivia) {
             is Result.Success -> {
                 _state.update {
                     it.copy(
-                        questions = lorem.data.results,
+                        questions = trivia.data.results,
                     )
                 }
             }
 
             is Result.Error -> {
-                println("ERROR FETCHING DATA:" + lorem.error)
+                println("ERROR FETCHING DATA:" + trivia.error)
             }
         }
 
@@ -68,53 +69,8 @@ class TriviaMainViewModel(
 
     fun onEvent(event: TriviaMainEvents) {
         when (event) {
-            TriviaMainEvents.PreviousQuestion -> {
-                _state.update {
-                    it.copy(
-                        currentQuestion = _state.value.currentQuestion - 1
-                    )
-                }
-            }
 
-            TriviaMainEvents.NextQuestion -> {
-                _state.update {
-                    it.copy(
-                        currentQuestion = _state.value.currentQuestion + 1
-                    )
-                }
-            }
-
-            is TriviaMainEvents.OnAnswerClicked -> {
-
-
-                val answeredQuestionsList = _state.value.answeredQuestions
-                val selectedAnswersList = _state.value.selectedAnswer.toMutableList()
-
-                selectedAnswersList[_state.value.currentQuestion] =
-                    event.value.selectedAnswer
-
-                val existingIndex =
-                    answeredQuestionsList.indexOfFirst { it.question == event.value.question }
-
-                if (existingIndex != -1) {
-                    // If exists, overwrite for the new one
-                    answeredQuestionsList[existingIndex] = event.value
-                } else {
-                    // If does not exists, add new
-                    answeredQuestionsList.add(event.value)
-                }
-
-                _state.update {
-                    it.copy(
-                        selectedAnswer = selectedAnswersList.toList()
-                    )
-                }
-
-            }
-
-            TriviaMainEvents.FinishTrivia -> {
-                
-            }
+            else -> {}
         }
     }
 
