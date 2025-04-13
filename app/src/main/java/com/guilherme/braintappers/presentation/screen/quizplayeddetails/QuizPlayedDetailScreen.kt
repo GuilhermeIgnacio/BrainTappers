b550444a6c244a7bb190f0b70f74b07ab044b169
GuilhermeIgnacio <guilhermeignacio002@gmail.com>
2025-04-12 22:16:34 -0300

    QuizPlayedDetailScreen UI revamp

--- app/src/main/java/com/guilherme/braintappers/presentation/screen/quizplayeddetails/QuizPlayedDetailScreen.kt
+++ app/src/main/java/com/guilherme/braintappers/presentation/screen/quizplayeddetails/QuizPlayedDetailScreen.kt
@@ -1,28 +1,40 @@
 package com.guilherme.braintappers.presentation.screen.quizplayeddetails
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.guilherme.braintappers.R
 import com.guilherme.braintappers.navigation.QuizzesPlayedScreen
 import com.guilherme.braintappers.presentation.screen.trivia.parseHtml
 import com.guilherme.braintappers.ui.theme.primaryColor
@@ -33,7 +45,7 @@ fun QuizPlayedDetailScreen(
     navController: NavController,
     questions: List<String>,
     userAnswers: List<String>,
-    correctAnswers: List<String>
+    correctAnswers: List<String>,
 ) {
 
     val foo = questions.zip(userAnswers).zip(correctAnswers)
@@ -42,6 +54,7 @@ fun QuizPlayedDetailScreen(
         modifier = Modifier
             .statusBarsPadding()
             .navigationBarsPadding(),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
     ) {
 
         item {
@@ -49,7 +62,7 @@ fun QuizPlayedDetailScreen(
                 onClick = { navController.navigate(QuizzesPlayedScreen) }
             ) {
                 Icon(
-                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                    imageVector = Icons.AutoMirrored.Filled.KeyboardArrowLeft,
                     contentDescription = "Return to Quizzes Played Screen"
                 )
             }
@@ -63,58 +76,87 @@ fun QuizPlayedDetailScreen(
 
             val isCorrect = userAnswer == correctAnswer
 
-            Surface(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(8.dp),
-                shadowElevation = 16.dp,
-                shape = RoundedCornerShape(16.dp),
-                color = if (isCorrect) primaryColor else Color(0xFFE40001),
-                contentColor = if (isCorrect) Color.Unspecified else Color.White
+            ElevatedCard(
+                modifier = Modifier.padding(12.dp),
+                onClick = {}
             ) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(16.dp)
-                ) {
+                Column {
                     Text(
-                        modifier = Modifier.fillMaxWidth(),
+                        modifier = Modifier
+                            .background(if (isCorrect) primaryColor else Color(0xFFE40001))
+                            .fillMaxWidth()
+                            .padding(16.dp),
                         text = question.parseHtml(),
                         fontFamily = poppinsFamily,
-                        fontWeight = FontWeight.Bold,
-                        textAlign = TextAlign.Center
+                        color = Color.White,
+                        style = MaterialTheme.typography.titleLarge
                     )
 
-                    Spacer(modifier = Modifier.height(8.dp))
+                    Spacer(modifier = Modifier.height(12.dp))
 
                     Text(
+                        modifier = Modifier.padding(horizontal = 16.dp),
                         text = "Your Answer",
                         fontFamily = poppinsFamily,
-                        fontWeight = FontWeight.Bold
+                        style = MaterialTheme.typography.bodySmall
                     )
 
-                    Text(
-                        text = userAnswer,
-                        fontFamily = poppinsFamily
-                    )
-
-                    if (!isCorrect) {
-                        Text(
-                            text = "Correct Answer",
-                            fontFamily = poppinsFamily,
-                            fontWeight = FontWeight.Bold
+                    Row(
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            modifier = Modifier.size(16.dp),
+                            painter = if (isCorrect) painterResource(R.drawable.circle_check_solid) else painterResource(R.drawable.circle_xmark_solid_red),
+                            tint = if (isCorrect) primaryColor else Color.Red,
+                            contentDescription = ""
                         )
 
+                        Spacer(modifier = Modifier.width(8.dp))
+
                         Text(
-                            text = correctAnswer,
+                            text = userAnswer,
                             fontFamily = poppinsFamily
                         )
                     }
 
+                    Spacer(modifier = Modifier.height(12.dp))
+
+                    if (!isCorrect) {
+
+                        Text(
+                            modifier = Modifier.padding(horizontal = 16.dp),
+                            text = "Correct Answer",
+                            fontFamily = poppinsFamily,
+                            style = MaterialTheme.typography.bodySmall
+                        )
+
+
+                        Row(
+                            modifier = Modifier.padding(horizontal = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                modifier = Modifier.size(16.dp),
+                                painter = painterResource(R.drawable.circle_regular),
+                                contentDescription = "",
+                                tint = primaryColor
+                            )
+
+                            Spacer(modifier = Modifier.width(8.dp))
+
+                            Text(
+                                text = correctAnswer,
+                                fontFamily = poppinsFamily
+                            )
+                        }
+                        Spacer(modifier = Modifier.height(12.dp))
+                    }
+
                 }
             }
-
         }
+
     }
 
 }
